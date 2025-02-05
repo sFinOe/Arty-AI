67850b09beefe00b29feb626a4a9a084a9fdd1e5
SfinOe <zakkikasmi@gmail.com>
2025-02-05 17:51:37 +0100

    FIX: netlify format es

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,14 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      // ...eslint({
-      //   include: ["src/**/*.+(js|jsx|ts)"],
-      //   exclude: ["node_modules/**", "dist/**"],
-      // }),
-      // enforce: "pre",
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
     },
-  ],
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       components: path.resolve(__dirname, "src/components"),
